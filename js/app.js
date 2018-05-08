414cce761edea85565dff2f0b32926cf37278bec
Hannah Sindorf <hannah@hsindorf.com>
2018-05-08 14:07:37 -0700

    added pseudocode for guessing game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// alert - welcome user to game and give instructions.
+
+// Prompt - ask question 1, and request yes or no, then change answer to lowercase.
+// if answer is correct - add a point, send user message that they were correct and current points
+// else - send them a message that they were not correct
+// console log answer to question 1 and correct or not
+
+// Prompt - ask question 2
+// if answer is correct - add a point, send user message that they were correct and current points
+// else - send them a message that they were not correct
+// console log answer to question 2 and correct or not
+
+// Prompt - ask question 3
+// if answer is correct - add a point, send user message that they were correct and current points
+// else - send them a message that they were not correct
+// console log answer to question 3 and correct or not
+
+// Prompt - ask question 4
+// if answer is correct - add a point, send user message that they were correct and current points
+// else - send them a message that they were not correct
+// console log answer to question 4 and correct or not
+
+// Prompt - ask question 5
+// if answer is correct - add a point, send user message that they were correct and current points
+// else - send them a message that they were not correct
+// console log answer to question 5 and correct or not
+
+// alert - return final score to user
