a66352958dae75bf0e115ed43be39b1195e61f72
Hannah Sindorf <hannah@hsindorf.com>
2018-05-08 17:31:58 -0700

    wrote guessing game. need Qs.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # guessing-game
 An "About Me" guessing game that asks the user yes/no questions about me (Hannah Sindorf) and gives a response based on whether or not the user is correct.
+
+## Observations
+- Silly syntax errors break everything
+- My actual code didn't follow the same logic as my pseudocode
+- it was easy to do all five questions once I wrote one
--- js/app.js
+++ js/app.js
@@ -1,30 +1,87 @@
 'use strict';
 
-// alert - welcome user to game and give instructions.
+// declare points variable
+var userPoints = 0;
 
-// Prompt - ask question 1, and request yes or no, then change answer to lowercase.
-// if answer is correct - add a point, send user message that they were correct and current points
+// alert - welcome user to game
+alert('Welcome! In this game, you will be given a fact and asked whether or not you think it is a real fact about me (Hannah!).');
+
+// prompt - ask name and store in userName variable
+var userName = prompt('To get started, please enter your name.');
+
+// alert - give instructions
+alert('Thanks, ' + userName + '! I hope you are excited to learn a little bit about me. To answer the following questions, please type \'Yes\' or \'No\'. Here comes the first question!');
+
+// prompts user to answer a question yes or no then changes the answer to lowercase
+var questionOne = prompt('Are dogs Hannah\'s favorite animal?').toLowerCase();
+
+// conditional statement checks if the response is correct
+// if correct, add a point, send user message that they were correct and current points
+if (questionOne === 'no') {
+  alert('Right! Hannah loves birds more than any other animal and has five of them. But she does also have a dog, so that doesn\'t mean she doesn\'t love dogs.');
+  userPoints++;
+}
+
 // else - send them a message that they were not correct
-// console log answer to question 1 and correct or not
+else {
+  alert('Wrong! Too bad! Hannah loves birds more than any other animal and has five of them. But she does also have a dog, so that doesn\'t mean she doesn\'t love dogs.');
+}
 
-// Prompt - ask question 2
-// if answer is correct - add a point, send user message that they were correct and current points
-// else - send them a message that they were not correct
-// console log answer to question 2 and correct or not
+// console log answer to question and correct or not
+console.log('User answered ' + questionOne + '. The answer was no.');
 
-// Prompt - ask question 3
-// if answer is correct - add a point, send user message that they were correct and current points
-// else - send them a message that they were not correct
-// console log answer to question 3 and correct or not
+alert('Okay, ' + userName + '. Moving on.');
 
-// Prompt - ask question 4
-// if answer is correct - add a point, send user message that they were correct and current points
-// else - send them a message that they were not correct
-// console log answer to question 4 and correct or not
+// questions below follow same format as above
 
-// Prompt - ask question 5
-// if answer is correct - add a point, send user message that they were correct and current points
-// else - send them a message that they were not correct
-// console log answer to question 5 and correct or not
+// Q2
+var questionTwo = prompt('').toLowerCase();
+if (questionTwo === '') {
+  alert('');
+  userPoints++;
+}
+else {
+  alert('Wrong!');
+}
+console.log('User answered ' + questionTwo + '. The answer was no.');
 
-// alert - return final score to user
+// Q3
+var questionThree = prompt('').toLowerCase();
+if (questionThree === '') {
+  alert('');
+  userPoints++;
+}
+else {
+  alert('Wrong!');
+}
+console.log('User answered ' + questionThree + '. The answer was no.');
+
+// Q4
+var questionFour = prompt('').toLowerCase();
+if (questionFour === '') {
+  alert('');
+  userPoints++;
+}
+else {
+  alert('Wrong!');
+}
+console.log('User answered ' + questionFour + '. The answer was no.');
+
+// Q5
+var questionFive = prompt('').toLowerCase();
+if (questionFive === '') {
+  alert('');
+  userPoints++;
+}
+else {
+  alert('Wrong!');
+}
+console.log('User answered ' + questionFive + '. The answer was no.');
+
+// conditional alert based on score that gives user final score
+if (userPoints === 5) {
+  alert('Wow! you got 100%! You must be some kind of mind reader!');
+}
+else {
+  alert('You only got ' + userPoints + '/5 questions right. We should hang out more!');
+}
